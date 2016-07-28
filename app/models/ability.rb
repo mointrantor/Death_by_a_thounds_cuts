075b28d6d728743ada91864f259e7056d3a42f05
Moin Haidar <moin.haidar@trantorinc.com>
2016-07-28 15:16:49 +0530

    Update Reset Password

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       if user.isAdmin
         can :manage, :all
       else
-        
+        can :update, User
       end
     #
     # The first argument to `can` is the action you are giving the user permission to do.
